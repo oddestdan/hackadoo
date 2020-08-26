b9c9784e246c6291290da792c7f7905a9f9f987b
Dan <oddestdan@gmail.com>
2020-08-26 12:28:36 +0300

    Update survey advancing logic, create skeleton for data fetching

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,95 +1,77 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Question.module.css';
-import Button from 'react-bootstrap/Button';
-import { roadmapApiUrl } from '../../environment';
-import axios from 'axios';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-const Question = () => {
-  const [data, setData] = useState({});
-  const [chekedCheckboxes, setChekedCheckboxes] = useState([]);
-  const [nextButtonDisabled, setNextButtonDisabled] = useState(false);
+const Question = ({ question, nextQuestion, isLastQuestion }) => {
+  const [checkboxes, setCheckboxes] = useState([]);
+  const [isButtonNext, setIsButtonNext] = useState(false);
+  const [areAllChecked, setAreAllChecked] = useState(false);
 
+  // Handle Checkboxes state
   useEffect(() => {
-    async function fetchData() {
-      const result = await axios(`${roadmapApiUrl}/firstSkill/front`);
-
-      const chekBoxArr = result.data.childrenSkills.map((skill) => {
-        return {
-          _id: skill._id,
-          isChecked: false,
-          title: skill.title,
-        };
-      });
-
-      setChekedCheckboxes(chekBoxArr);
-
-      setData(result.data);
+    if (question.childrenSkills) {
+      const checkboxesData = question.childrenSkills.map(({ _id, title }) => ({
+        _id,
+        title,
+        isChecked: false,
+      }));
+      setCheckboxes(checkboxesData);
     }
 
-    fetchData();
-  }, [data.childrenSkills]);
+    // handleButtonValue();
+  }, [question.childrenSkills]);
 
-  const fetchNextQuestion = () => {
-    async function fetchData() {
-      const result = await axios(`${roadmapApiUrl}/skill/front/${data.nextId}`);
+  // Handle All Checked state
+  useEffect(() => {
+    setAreAllChecked(checkboxes.every((el) => el.isChecked === true));
+  }, [checkboxes]);
 
-      setData(result.data);
-    }
+  // Handle button value
+  useEffect(() => {
+    setIsButtonNext(areAllChecked && !isLastQuestion);
+  }, [areAllChecked, isLastQuestion]);
 
-    fetchData();
-  };
+  // TODO: if button is !isButtonNext -> need to create logic to send survey data
 
-  const handleChecked = (e) => {
+  const handleCheckboxCheck = (e) => {
     if (e.target.checked) {
-      setChekedCheckboxes(
-        chekedCheckboxes.map((checkbox) =>
-          checkbox._id === e.target.id ? { isChecked: true } : checkbox
+      setCheckboxes(
+        checkboxes.map((checkbox) =>
+          checkbox._id === e.target.id
+            ? { ...checkbox, isChecked: true }
+            : checkbox
         )
       );
-    }
-
-    handleButtonDisabled();
-  };
-
-  const handleButtonDisabled = () => {
-    if (!chekedCheckboxes.some((el) => el.isChecked === false)) {
-      setNextButtonDisabled(true);
     }
   };
 
   return (
     <div className={styles.container}>
-      <p className={styles.question}>Do you know - {data.title}?</p>
-      <p>Check technologies:</p>
+      <p className={styles.question}>Do you know - {question.title}?</p>
+      <p>Check what you know:</p>
       <Form className={styles.form}>
-        {data.childrenSkills &&
-          data.childrenSkills.map((skill) => (
-            <div key={skill.title} className={styles['checkbox-container']}>
+        {question.childrenSkills &&
+          question.childrenSkills.map((skill) => (
+            <div key={skill._id} className={styles['checkbox-container']}>
               <Form.Check
                 inline
                 label={skill.title}
                 type={'checkbox'}
                 id={skill._id}
                 className={styles.checkbox}
-                onChange={handleChecked}
+                onChange={handleCheckboxCheck}
               />
             </div>
           ))}
       </Form>
-      <div>
-        <Button variant="secondary" className={styles.button}>
-          Previous
-        </Button>
-        <Button
-          variant="success"
-          className={styles.button}
-          onClick={fetchNextQuestion}
-          disabled={nextButtonDisabled}
-        >
-          Next
-        </Button>
-      </div>
+      <Button
+        variant={isButtonNext ? 'success' : 'primary'}
+        onClick={nextQuestion}
+        className={styles.button}
+      >
+        {isButtonNext ? 'Next' : 'Finish'}
+      </Button>
     </div>
   );
 };
--- src/components/Survey/Survey.jsx
+++ src/components/Survey/Survey.jsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import Question from '../Question/Question';
-import styles from './Survey.module.css';
+// import styles from './Survey.module.css';
+import { roadmapApiUrl } from '../../environment';
 
 const Survey = () => {
+  // const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState({});
+  const [questionId, setQuestionId] = useState(null);
+  const [isLastQuestion, setIsLastQuestion] = useState(false);
+
+  const fetchSkill = (id = null) => {
+    // TODO: get this value from redux store after clicking on path from Cabinet
+    const path = 'back';
+    let fetchUrl = `${roadmapApiUrl}/firstSkill/${path}`;
+    if (id) {
+      fetchUrl = `${roadmapApiUrl}/skill/${path}/${id}`;
+    }
+    fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((question) => {
+        // TODO: save/add to redux store progress
+        setQuestion(question);
+      });
+  };
+
+  useEffect(() => {
+    fetchSkill(questionId);
+  }, [questionId]);
+
+  const handleNextQuestion = () => {
+    if (question.nextId) {
+      setQuestionId(question.nextId);
+    } else {
+      stopSurvey();
+    }
+  };
+
+  const stopSurvey = () => {
+    setIsLastQuestion(true);
+    // TODO: take progress from redux
+    // sendProgressToBackend();
+  };
+
   return (
-    <div className={styles.container}>
-      <Question />
-    </div>
+    <>
+      <Carousel
+        // activeIndex={index}
+        controls={false}
+        indicators={false}
+        interval={null}
+      >
+        <Carousel.Item key={question.id}>
+          <Question
+            question={question}
+            nextQuestion={handleNextQuestion}
+            isLastQuestion={isLastQuestion}
+          />
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 };
 
